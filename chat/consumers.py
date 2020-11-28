70c16092e857e2225589f1afa06c84e08131698f
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-11-28 10:27:51 +0000

    feat: update routing, consumer and add dev backend for messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,10 +10,13 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
+
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.room_name
+            self.channel_name
         )
+
+        await self.accept()
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -35,3 +38,5 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    pass
